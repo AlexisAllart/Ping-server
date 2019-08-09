3fbf363caba25985b10edf164b9596331ffdb2ca
AlexisAllart <alexis.allart@gmail.com>
2019-08-09 09:52:12 +0200

    port

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // DEPENDENCIES
 // Express
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
 // Body-parser
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // VARS & CONSTS
-const user=require('./routes/user.route');
-const company=require('./routes/company.route');
-const companyUser=require('./routes/companyUser.route');
-const offer=require('./routes/offer.route');
-const contractType=require('./routes/contractType.route');
-const keyWord=require('./routes/keyWord.route');
-const role=require('./routes/role.route');
-const status=require('./routes/status.route');
-const tag=require('./routes/tag.route');
-const ping=require('./routes/ping.route');
-const selection=require('./routes/selection.route');
+const user = require('./routes/user.route');
+const company = require('./routes/company.route');
+const companyUser = require('./routes/companyUser.route');
+const offer = require('./routes/offer.route');
+const contractType = require('./routes/contractType.route');
+const keyWord = require('./routes/keyWord.route');
+const role = require('./routes/role.route');
+const status = require('./routes/status.route');
+const tag = require('./routes/tag.route');
+const ping = require('./routes/ping.route');
+const selection = require('./routes/selection.route');
 
 require('events').EventEmitter.defaultMaxListeners = 50;
 
@@ -36,10 +36,9 @@ app.use('/tag', tag);
 app.use('/ping', ping);
 app.use('/selection', selection);
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Ping status = ONLINE');
 });
-  
 
-app.listen(9090);
+app.listen(process.env.PORT || 9090);
 // END
