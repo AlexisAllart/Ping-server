d87e4b35d40740e92483eea05d0533e0fe924a3d
AlexisAllart <alexis.allart@gmail.com>
2019-09-12 22:36:20 +0200

    db fix

--- migrations/POPULATE-DEV-07-selection.js
+++ migrations/POPULATE-DEV-07-selection.js
@@ -2,56 +2,56 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Selections', [{
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 3,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 4,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 5,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 6,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 7,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 8,
@@ -65,56 +65,56 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 2,
+      companyUser_id: 1,
       tag_id: 1,
       company_id: 1,
       user_id: 10,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 4,
+      companyUser_id: 2,
       tag_id: 1,
       company_id: 2,
       user_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 5,
+      companyUser_id: 2,
       tag_id: 1,
       company_id: 2,
       user_id: 3,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 5,
+      companyUser_id: 2,
       tag_id: 1,
       company_id: 2,
       user_id: 5,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 6,
+      companyUser_id: 2,
       tag_id: 1,
       company_id: 2,
       user_id: 6,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 7,
+      companyUser_id: 3,
       tag_id: 1,
       company_id: 3,
       user_id: 10,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 8,
+      companyUser_id: 3,
       tag_id: 1,
       company_id: 3,
       user_id: 12,
       createdAt: new Date(),
       updatedAt: new Date()
     }, { 
-      companyUser_id: 9,
+      companyUser_id: 3,
       tag_id: 1,
       company_id: 3,
       user_id: 8,
